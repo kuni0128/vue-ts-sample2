17302bf653785addc5c39942b6563e4f9cdbe231
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2020-02-29 18:17:03 +0900

    create trello page

---
+++ src/views/Trello.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="trello">
+    <h1>This is an trello page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Trello extends Vue {}
+</script>
