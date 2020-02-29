d3d174d4b68c7a6eef6b3aa81726c9845a4f7ff4
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2020-02-29 18:36:17 +0900

    add types.ts and initialData.ts

---
+++ src/initialData.ts
@@ -0,0 +1,23 @@
+import { IList } from '@/types';
+
+export function createInitialLists(): IList[] {
+  return [
+    {
+      id: 1,
+      name: 'TODO',
+      cards: [
+        { id: 1, text: 'タスク1' },
+        { id: 2, text: 'タスク2' }
+      ]
+    },
+    {
+      id: 2,
+      name: '進行中',
+      cards: [
+        { id: 3, text: 'タスク3' },
+        { id: 4, text: 'タスク4' },
+        { id: 5, text: 'タスク5' }
+      ]
+    }
+  ];
+}
