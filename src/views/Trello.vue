c8241b9a9e52ee5612586c37da0cbcf36e51f35f
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2020-03-01 21:32:37 +0900

    fix for eslint

--- src/views/Trello.vue
+++ src/views/Trello.vue
@@ -21,7 +21,7 @@ import { createInitialLists } from '@/initialData';
 export default class Trello extends Vue {
   private lists: IList[] = createInitialLists();
   private listCreatedCount = this.lists.length;
-  private cardCreatedCount = this.lists.map(list => list.cards.length).reduce((a, x) => a += x);
+  private cardCreatedCount = this.lists.map((list) => list.cards.length).reduce((a, x) => a += x);
 
   private addList(event: Event & { currentTarget: HTMLInputElement }): void {
     ++this.listCreatedCount;
@@ -35,8 +35,8 @@ export default class Trello extends Vue {
   }
 
   private addCard({ listId, text }: IAddCardEvent): void {
-    const list = this.lists.find(list => list.id === listId);
-    if (list === undefined) return;
+    const list = this.lists.find((l) => l.id === listId);
+    if (list === undefined) { return; }
     ++this.cardCreatedCount;
     const newCard = { id: this.cardCreatedCount, text };
     list.cards.push(newCard);
